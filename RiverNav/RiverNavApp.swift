14b776733870e19284ae13a298f13cb446361ec1
Samoilov O.G. <samojlovog@gmail.com>
2026-05-11 18:19:35 +0300

    Initial Commit

---
+++ RiverNav/RiverNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiverNavApp.swift
+//  RiverNav
+//
+//  Created by Oleg Samoylov on 11.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct RiverNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
